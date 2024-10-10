9be405e85a85b6164d223c4a1de0cd852738eefd
Maarten <maartenzam@gmail.com>
2024-10-10 20:11:25 +0200

    Viz rearranged, Other countries

--- src/lib/components/Timeline.svelte
+++ src/lib/components/Timeline.svelte
@@ -16,9 +16,15 @@
 	const margins = {
 		top: 0,
 		right: 24,
-		bottom: 28,
+		bottom: 0,
 		left: 120
 	};
+	const marginsKeyEvents = {
+		top: 0,
+		right: 24,
+		bottom: 38,
+		left: 120
+	}
 
 	let width;
 	let height = 200;
@@ -29,11 +35,12 @@
 		.range([0.2, 1])
 	$: ticks = xScale.ticks(5);
 
-	const actorNations = ['Key event', 'China', 'Iran', 'North Korea', 'Russia'];
+	const actorNations = ['Other', 'China', 'Iran', 'North Korea', 'Russia'];
 	const colors = ['#555555', '#bf0a0a', '#0f8a0f', '#8a4d0f', '#0f4c8a'];
 
 	let yScale = scalePoint(actorNations, [height - margins.bottom - margins.top, 0]).padding(0.5);
 	let colorScale = scaleOrdinal(actorNations, colors);
+	const keyEventColor = "#555555"
 
 	$: displayCountryMetrics = $actorNationFilter.filter(d => d.selected).map(d => d.name)
 	$: filteredMetrics = metrics.filter(d => displayCountryMetrics.includes(d.country))
@@ -87,28 +94,12 @@
 						fill={colorScale(nation)}>{nation}</text
 					>
 				{/each}
-				{#each ticks as tick}
-					<line
-						x1={xScale(tick)}
-						x2={xScale(tick)}
-						y1={height - margins.bottom - margins.top}
-						y2={height - margins.bottom - margins.top + 10}
-						stroke={'#bbbbbb'}
-						stroke-width={1}
-					></line>
-					<text
-						class="time-axis-tick-label"
-						x={xScale(tick)}
-						y={height - margins.bottom - margins.top + 24}
-						text-anchor={'middle'}>{utcFormat('%b')(tick)}</text
-					>
-				{/each}
 				{#each cases as attrCase}
 					{#if attrCase.show}
 						<a href={'#case-' + attrCase.attribution_id} transition:fade>
 							<Bubble
 								cx={xScale(new Date(attrCase.attribution_date))}
-								cy={yScale(attrCase.actor_nation[0])}
+								cy={actorNations.includes(attrCase.actor_nation[0]) ? yScale(attrCase.actor_nation[0]) : yScale('Other')}
 								r={6}
 								fill={colorScale(attrCase.actor_nation[0])}
 								stroke={'#ffffff'}
@@ -123,36 +114,10 @@
 						</a>
 					{/if}
 				{/each}
-				{#if xScale}
-					{#each events as event}
-						<!--line
-							x1={xScale(event.date)}
-							x2={xScale(event.date)}
-							y1={yScale('Key event')}
-							y2={0}
-							stroke={colorScale('Key event')}
-							stroke-width={2}
-							opacity={0.5}
-						></line-->
-						<Square
-							x={xScale(event.date)}
-							y={yScale('Key event')}
-							width={10}
-							fill={'#ffffff'}
-							stroke={colorScale('Key event')}
-							strokeWidth={1}
-							ttContent={`<p style='font-weight; bold;'>${event.Title}</p>
-										<p>${event.Description}</p>`}
-							bind:tooltipContent
-							bind:tooltipX
-							bind:tooltipY
-							bind:showTooltip
-						></Square>
-					{/each}
-				{/if}
 			</g>
 		{/if}
 	</svg>
+
 	<svg {width} height={height}>
 		{#if xScale}
 			<g transform={`translate(${margins.left},${margins.top})`}>
@@ -160,6 +125,63 @@
 					{#each stackedMetrics as serie}
 						<path d={areaGenerator(serie)} stroke={'white'} stroke-width={1} fill={colorScale(serie.key)}>
 						</path>
+					{/each}
+				{/if}
+			</g>
+		{/if}
+	</svg>
+
+	<svg {width} height={height/2}>
+		{#if xScale}
+			<g transform={`translate(${marginsKeyEvents.left},${marginsKeyEvents.top})`}>
+					<line
+						x1={0}
+						x2={width}
+						y1={32}
+						y2={32}
+						style:stroke={keyEventColor}
+						stroke-width={32}
+						opacity={0.1}
+					></line>
+					<text
+						class="country-label"
+						x={-10}
+						y={32 + 4}
+						text-anchor={'end'}
+						fill={keyEventColor}></text
+					>
+				{#each ticks as tick}
+					<line
+						x1={xScale(tick)}
+						x2={xScale(tick)}
+						y1={height/2 - marginsKeyEvents.bottom}
+						y2={height/2 - marginsKeyEvents.bottom + 10}
+						stroke={'#777777'}
+						stroke-width={1}
+					></line>
+					<text
+						class="time-axis-tick-label"
+						x={xScale(tick)}
+						y={height/2 - marginsKeyEvents.bottom + 24}
+						text-anchor={'middle'}>{utcFormat('%b')(tick)}</text
+					>
+				{/each}
+				{#if xScale}
+					{#each events as event}
+						<Square
+							x={xScale(event.date)}
+							y={32}
+							width={12}
+							fill={keyEventColor}
+							stroke={'#ffffff'}
+							strokeWidth={2}
+							ttContent={`<p style='font-weight; bold;'>${event.Title}</p>
+										<p>${event.Description}</p>`}
+							bind:tooltipContent
+							bind:tooltipX
+							bind:tooltipY
+							bind:showTooltip
+						></Square>
 					{/each}
 				{/if}
 			</g>
@@ -179,8 +201,8 @@
 		font-size: 0.9rem;
 	}
 	.time-axis-tick-label {
-		font-size: 0.8rem;
-		fill: #888888;
+		font-size: 0.9rem;
+		fill: #777777;
 	}
 	.event-title {
 		font-weight: bold;
