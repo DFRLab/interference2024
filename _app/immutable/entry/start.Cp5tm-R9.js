94d9c1cf693517ceaeb199396008fd2779b0af5a
DFRLab <DFRLab@users.noreply.github.com>
2024-10-12 16:02:29 +0000

    deploy: b4d46e331515135fd852a09e78fd168614b541d7

---
+++ _app/immutable/entry/start.Cp5tm-R9.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Cu_RPa2W.js";export{t as start};
--- src/stores/scales.js
+++
@@ -1,6 +0,0 @@
-import { writable, readable } from 'svelte/store';
-
-export const timeScale = writable();
-//export const attributionScoreScale = writable();
-
-//export const scaleFactor = readable(window.devicePixelRatio || 1);
