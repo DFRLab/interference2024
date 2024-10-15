56cb2be36478c898529c403235ac492cca6f1e58
Maarten <maartenzam@gmail.com>
2024-10-15 09:51:51 +0200

    Lorem ipsum

--- google.config.js
+++ google.config.js
@@ -1,7 +1,7 @@
 export default [
 	{
-		"id": "1tSZ6hKWKR0u3pIxvm3L_6SO3ab2yqjM8ww7GMBU3EwE",
-		//"id": "1sDuhhouZ1IYsi355slAtY2SeF6781vO8AvhVL-EE_3A",
+		//"id": "1tSZ6hKWKR0u3pIxvm3L_6SO3ab2yqjM8ww7GMBU3EwE",
+		"id": "1sDuhhouZ1IYsi355slAtY2SeF6781vO8AvhVL-EE_3A",
 		"filepath": "src/data/copy.json"
 	}
 ]
